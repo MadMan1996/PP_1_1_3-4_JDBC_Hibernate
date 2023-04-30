ca01b42bc319487e84c1de74c23e231a92394ffe
Maksim <bogdanenko1996@gmail.com>
2023-04-30 17:27:10 +0300

    JDBC connection implementation

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,11 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,21 +13,21 @@ import java.sql.SQLException;
 public class Util {
     // реализуйте настройку соеденения с БД
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/mysql";
-    private static final String DB_USERNAME = "NhbyblfnjNj,fuj34!";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/world";
+    private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "NhbyblfnjNj,fuj34!";
 
     public static Connection getConnection() {
         Connection connection = null;
-        try {
-            Class.forName(DB_DRIVER);
-            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            System.out.println("DB is connected");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+        while (connection == null) {
+            try {
+                Class.forName(DB_DRIVER);
+                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("DB connection failed");
+            }
         }
-
-        return connection;
+       return connection;
     }
 
 }
