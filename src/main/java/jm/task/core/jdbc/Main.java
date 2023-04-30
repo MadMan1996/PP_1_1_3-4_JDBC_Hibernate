33c19682165e59157e7fd9201c8f22e3e4746646
Maksim <bogdanenko1996@gmail.com>
2023-04-30 17:30:29 +0300

    Test for UserService

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws Exception {
+        UserService userService = new UserServiceImpl();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser("Andrey", "Petrov", (byte) 45);
+        userService.saveUser("Artem", "Ivanov", (byte) 4);
+        System.out.println(userService.getAllUsers());
+        userService.removeUserById(1);
+        userService.cleanUsersTable();
     }
 }
