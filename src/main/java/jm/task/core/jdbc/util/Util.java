f45c4b1dd5a12d8fff464fc45beda307625bf784
Maksim <bogdanenko1996@gmail.com>
2023-05-03 17:34:22 +0300

    Config Hibernate connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,19 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.bytecode.enhance.internal.bytebuddy.EnhancerImpl;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
 
 public final class Util {
     // реализуйте настройку соеденения с БД
@@ -17,24 +22,28 @@ public final class Util {
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "NhbyblfnjNj,fuj34!";
 
+    private static final String DB_HIBER_DIALECT = "org.hibernate.dialect.MySQL5Dialect";
+
     private static Connection connection;
+
+    private static SessionFactory sessionFactory;
 
     static {
         loadClass();
     }
+
     private Util() {
 
     }
 
     private static void loadClass() {
-        try{
+        try {
             Class.forName(DB_DRIVER);
 
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
-
 
 
     public static Connection getConnection() {
@@ -45,7 +54,35 @@ public final class Util {
                 throw new RuntimeException(e);
             }
         }
-       return connection;
+        return connection;
     }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, DB_DRIVER);
+            settings.put(Environment.URL, DB_URL);
+            settings.put(Environment.USER, DB_USERNAME);
+            settings.put(Environment.PASS, DB_PASSWORD);
+            settings.put(Environment.DIALECT, DB_HIBER_DIALECT);
+
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.HBM2DDL_AUTO, "validate");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+            configuration.setProperties(settings);
+
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
+    }
+
+
 
 }
