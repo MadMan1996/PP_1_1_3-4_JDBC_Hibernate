d869bf7e7141532332e2d4309f8818c6ef9c5a3e
Maksim <bogdanenko1996@gmail.com>
2023-05-03 11:08:41 +0300

    Singleton pattern for JDBC Connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -10,21 +10,39 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Util {
+public final class Util {
     // реализуйте настройку соеденения с БД
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/world";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "NhbyblfnjNj,fuj34!";
 
+    private static Connection connection;
+
+    static {
+        loadClass();
+    }
+    private Util() {
+
+    }
+
+    private static void loadClass() {
+        try{
+            Class.forName(DB_DRIVER);
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
     public static Connection getConnection() {
-        Connection connection = null;
-        while (connection == null) {
+        if (connection == null) {
             try {
-                Class.forName(DB_DRIVER);
                 connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            } catch (ClassNotFoundException | SQLException e) {
-                System.out.println("DB connection failed");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
         }
        return connection;
